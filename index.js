23cfdf0eda8c0bd8b51ea12dc0468d5e23678941
mikecroall <mikebcroall@gmail.com>
2017-01-26 16:27:19 +0000

    Filter demo

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#trcAge").slider({
+        id: "trcAge"
+    });
+    $("#trcAge").on("slide", function(slideEvt) {
+        // console.log("Age range selected", slideEvt.value);
+        $("#trcAgeSelection").text(slideEvt.value.join(" - "));
+    });
+});
