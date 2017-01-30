c6f3f5324174856d84320aaeccb09170af304068
mikecroall <mikebcroall@gmail.com>
2017-01-30 12:14:16 +0000

    Console log on slider change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         id: "trcAge"
     });
     $("#trcAge").on("slide", function(slideEvt) {
-        // console.log("Age range selected", slideEvt.value);
+        console.log("Age range selected", slideEvt.value);
         $("#trcAgeSelection").text(slideEvt.value.join(" - "));
     });
 });
