6bd66d1f29a7d5960572740ff550d41000ba31d1
mikecroall <mikebcroall@gmail.com>
2017-01-30 16:26:14 +0000

    Minor tweaks

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
+    // Create age slider
     $("#trcAge").slider({
         id: "trcAge"
     });
+
+    // Attach age slider label
+    $("#trcAgeSelection").text(slideEvt.value.join(" - "));
     $("#trcAge").on("slide", function(slideEvt) {
         // console.log("Age range selected", slideEvt.value);
         $("#trcAgeSelection").text(slideEvt.value.join(" - "));
