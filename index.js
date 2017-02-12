57b42155881df6f32e6251597ee657390580a5e5
Mitchel <mitchel.starkie@gmail.com>
2017-02-12 17:22:52 +0000

    server authentication

--- index.js
+++ index.js
@@ -1,29 +1,70 @@
 var express = require('express')
-var mongodb = require('mongodb')
+var db = require('mongodb')
 var passport = require('passport')
-var passportHTTP = require('passport-http')
+var Strategy = require('passport-local').Strategy
+var connectEnsure = require('connect-ensure-login')
+
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err) }
+      if (!user) { return cb(null, false) }
+      if (user.password != password) { return cb(null, false) }
+      return cb(null, user)
+    })
+  }
+))
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id)
+})
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err) }
+    cb(null, user)
+  })
+})
 
 var app = express()
 
-const PUBLIC_DIRECTORY = 'public'
-const PATH_PREFIX = __dirname + '/' + PUBLIC_DIRECTORY
+//app.use('/pdf', express.static(__dirname + '/node_modules/pdfmake/build'))
+app.use(express.static('public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 
-// console.log(PATH_PREFIX)
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false }
+))
 
-app.use('/pdf', express.static(__dirname + '/node_modules/pdfmake/build'));
-app.use(express.static(PUBLIC_DIRECTORY));
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/', function(req, res) {
-  res.sendFile(PATH_PREFIX + '/index.html')
+app.get('/',
+  connectEnsure.ensureLoggedIn('/login'),
+  function(req, res) {
+    res.render('index')
 })
 
-app.get('/login', function(req, res) {
-  res.sendFile(PATH_PREFIX + '/login.html')
+app.get('/login',
+  function(req, res) {
+    res.render('login')
 })
+
+app.post('/login',
+  passport.authenticate(
+    'local',
+    {
+      successReturnToOrRedirect: '/',
+      failureRedirect: '/login' 
+    }
+  )
+);
 
 //The 404 Route (ALWAYS Keep this as the last route)
 app.get('*', function(req, res) {
-  // console.log(req)
   res.status(404).send('uh')
 });
 
